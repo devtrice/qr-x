07b8b971d3eb653fac39f6ea1a09027dfc990765
ZeyarPaing <zeyarpaing@protonmail.com>
2023-11-25 13:18:22 +0630

    chore: width of render

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import QRX from '@qr-x/react'
 export default function Page() {
   return (
     <main className='flex items-center justify-center w-screen h-screen'>
-      <div className='max-w-xl w-full flex gap-x-10'>
+      <div className='max-w-2xl w-full flex gap-x-10'>
         <QRX data='https://github.com/devtrice' shape='rect' />
         <QRX data='https://github.com/devtrice' shape='circle' />
       </div>
--- packages/react/index.tsx
+++ packages/react/index.tsx
@@ -7,7 +7,8 @@ export default function QRX({ shape = 'rect', ...options }: Props) {
   const matrix = getMatrix(options)
   const { tag, props } = shapes[shape]
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' viewBox={`0 0 ${matrix.length} ${matrix.length}`} width={500} height={500}>
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox={`0 0 ${matrix.length} ${matrix.length}`} width='100%' height='100%'>
+      {/* Safari won't render SVGs without width and height */}
       {matrix.map((row, i) => row.map(({ isON }, j) => createElement(tag, { ...props(i, j), fill: isON ? '#000' : '#fff' })))}
     </svg>
   )
