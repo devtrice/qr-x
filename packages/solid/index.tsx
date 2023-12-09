54723d85babc9a47be9464e8c1ce9a19f2710132
ZeyarPaing <zeyarpaing@protonmail.com>
2023-12-09 14:31:09 +0800

    fix: reactivity in solid

--- packages/solid/index.tsx
+++ packages/solid/index.tsx
@@ -1,53 +1,56 @@
 import { getSVGData, Options } from '@qr-x/core'
-import { For, JSX } from 'solid-js'
+import { createMemo, For, JSX, splitProps } from 'solid-js'
 import { Dynamic } from 'solid-js/web'
 
 type Props = Options & JSX.SvgSVGAttributes<SVGSVGElement>
 
-export default function QRX({ data, level, shapes, image, gradient: $gradient, fillImage, ...rest }: Props) {
-  const { ids, fills, paths, length, markers, gradient, eyeItems, isMasked } = getSVGData({
-    data,
-    level,
-    image,
-    shapes,
-    gradient: $gradient,
-    fillImage,
-  })
+export default function QRX($props: Props) {
+  const [props, rest] = splitProps($props, ['data', 'level', 'shapes', 'image', 'gradient', 'fillImage'])
+
+  const svg = createMemo(() => getSVGData(props))
 
   const group = (
-    <g fill={fills.path}>
-      <path d={paths.body} />
-      <For each={eyeItems}>{item => <For each={markers}>{marker => <use href={`#${ids[item]}`} {...marker} />}</For>}</For>
+    <g fill={svg().fills.path}>
+      <path d={svg().paths.body} />
+      <For each={svg().eyeItems}>
+        {item => <For each={svg().markers}>{marker => <use href={`#${svg().ids[item]}`} {...marker} />}</For>}
+      </For>
     </g>
   )
 
   return (
-    <svg width='100%' {...rest} viewBox={`0 0 ${length} ${length}`}>
-      {isMasked ? (
-        <g>
-          <mask id='mask'>{group}</mask>
-          <rect x='0' y='0' width='100%' height='100%' fill={fills.rect} mask="url('#mask')" />
-        </g>
-      ) : (
-        group
-      )}
-
-      <defs>
-        <For each={eyeItems}>{item => <path id={ids[item]} d={paths[item]} />}</For>
-
-        {gradient ? (
-          <Dynamic component={gradient.isLinearGradient ? 'linearGradient' : 'radialGradient'} {...gradient.attributes}>
-            <For each={gradient.colors}>{({ color, offset }) => <stop offset={offset} stop-color={color} />}</For>
-          </Dynamic>
+    <>
+      <p>{svg().ids.eyeball}</p>
+      <svg width='100%' {...rest} viewBox={`0 0 ${svg().length} ${svg().length}`}>
+        {svg().isMasked ? (
+          <g>
+            <mask id='mask'>{group}</mask>
+            <rect x='0' y='0' width='100%' height='100%' fill={svg().fills.rect} mask="url('#mask')" />
+          </g>
         ) : (
-          fillImage && (
-            <pattern id={ids.image} patternUnits='userSpaceOnUse' width='100%' height='100%'>
-              <image href={fillImage} x='0' y='0' width='100%' height='100%' preserveAspectRatio='xMidYMid slice' />
-            </pattern>
-          )
+          group
         )}
-      </defs>
-    </svg>
+
+        <defs>
+          <For each={svg().eyeItems}>{item => <path id={svg().ids[item]} d={svg().paths[item]} />}</For>
+
+          {svg().gradient ? (
+            <Dynamic
+              component={svg().gradient?.isLinearGradient ? 'linearGradient' : 'radialGradient'}
+              {...svg().gradient?.attributes}
+            >
+              <For each={svg().gradient?.colors}>{({ color, offset }) => <stop offset={offset} stop-color={color} />}</For>
+            </Dynamic>
+          ) : (
+            props.fillImage && (
+              <pattern id={svg().ids.image} patternUnits='userSpaceOnUse' width='100%' height='100%'>
+                <image href={props.fillImage} x='0' y='0' width='100%' height='100%' preserveAspectRatio='xMidYMid slice' />
+              </pattern>
+            )
+          )}
+        </defs>
+      </svg>
+    </>
   )
 }
 
