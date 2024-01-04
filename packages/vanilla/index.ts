67839da18e20fd39834c1169b731153ed02e9f74
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-01-04 16:29:30 +0630

    feat: vanilla qr

--- packages/vanilla/index.ts
+++ packages/vanilla/index.ts
@@ -1,14 +1,13 @@
 import { getSVGData, Options } from '@qr-x/core'
 
 export type Props = Options & { [key: string]: unknown; parent?: string }
-// TODO: Typecast the { [key: string]: unknown } to intellisense the svg DOM attributes
+
+const objectToAtr = (obj: Object) =>
+  Object.entries(obj)
+    .map(([k, v]) => k + "='" + v + "'")
+    .join(' ')
 
 export default function createQRX({ data, level, shapes, image, gradient: $gradient, fillImage, ...rest }: Props) {
-  // Your codes here
-  // Rendering logics can be found at packages/react/index.tsx
-  // This function should return the svg element.
-  // If `parent` element exist render inside that element.
-
   const { ids, fills, paths, length, markers, gradient, eyeItems, isMasked } = getSVGData({
     data,
     level,
@@ -18,7 +17,52 @@ export default function createQRX({ data, level, shapes, image, gradient: $gradi
     fillImage,
   })
 
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  const uses = eyeItems.map(item =>
+    markers.map(marker => `<use href='#${ids[item]}' xlink:href='#${ids[item]}' ${objectToAtr(marker)}/>`),
+  )
+
+  const group = `
+    <g fill=${fills.path}>
+      <path d='${paths.body}' />
+      ${uses}
+    </g>`
+
+  const stops = gradient?.colors
+    ? gradient.colors.map(({ color, offset }) => `<stop offset='${offset}' stop-color='${color}' />`)
+    : []
+
+  const view = gradient
+    ? gradient.isLinearGradient
+      ? `<linearGradient ${objectToAtr(gradient.attributes)}>${stops}</linearGradient>`
+      : `<radialGradient ${objectToAtr(gradient.attributes)}>${stops}</radialGradient>`
+    : fillImage
+      ? `<pattern id=${ids.image} patternUnits='userSpaceOnUse' width='100%' height='100%'>
+                <image
+                  x='0'
+                  y='0'
+                  width='100%'
+                  height='100%'
+                  href='${fillImage}'
+                  xlink:href='${fillImage}'
+                  preserveAspectRatio='xMidYMid slice'
+                />
+        </pattern>`
+      : ''
+
+  const groupView = isMasked
+    ? `<g>
+          <mask id='mask'>${group}</mask>
+          <rect x='0' y='0' width='100%' height='100%' fill=${fills.rect} mask="url('#mask')" />
+       </g>`
+    : group
+
+  const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='100%' viewBox='0 0 ${length} ${length}' ${objectToAtr(rest)}>
+                ${groupView}
+                <defs>
+                  ${eyeItems.map(item => `<path id='${ids[item]}' d='${paths[item]}' />`)}
+                </defs>
+                ${view}
+              </svg>`
 
   return svg
 }
