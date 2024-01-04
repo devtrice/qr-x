8834b92daa454e6047afcaf9ba98ded71213db4d
Augustin Joseph <augustinjoseph@Aung-Bo-Bo-Tuns-Laptop.local>
2024-01-04 21:38:32 +0630

    update: yarn to pnpm

--- packages/react/index.stories.tsx
+++ packages/react/index.stories.tsx
@@ -1,5 +1,5 @@
+import { Default, FillImage, LinearGradient, RadialGradient, defaultMeta, renderMultiple } from '@common/stories'
 import type { Meta } from '@storybook/react'
-import { Default, FillImage, LinearGradient, RadialGradient, defaultMeta, renderMultiple } from 'stories'
 import QRX from './index'
 
 const Multiple = () => <div className='grid'>{renderMultiple(QRX)}</div>
