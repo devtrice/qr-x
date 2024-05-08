84ae14b9462ede460dd04ea77e262b2e3cf51c81
Yin-Lynn-Htun <yinlynnhtun@rezeve.com>
2024-05-08 15:49:48 +0700

    Added ts support

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -1,28 +1,33 @@
-import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import * as path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'index.ts'),
+      entry: path.resolve(__dirname, 'index.ts'),
       name: 'QrXVue',
-      // the proper extensions will be added
       fileName: 'qr-x-vue',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
