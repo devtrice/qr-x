301f1f94ebaf796e56da5f0f0d753d2e4909d348
Zeyar Paing <zeyarpaing@proton.me>
2024-06-18 23:56:44 +0700

    feat: update solid

--- packages/solid/index.tsx
+++ packages/solid/index.tsx
@@ -5,42 +5,43 @@ import { Dynamic } from 'solid-js/web'
 type Props = Options & JSX.SvgSVGAttributes<SVGSVGElement>
 
 export default function QRX($props: Props) {
-  const [props, rest] = splitProps($props, ['data', 'level', 'shapes', 'gradient'])
-
+  const [props, rest] = splitProps($props, ['data', 'level', 'shapes', 'gradient', 'fillImage', 'fillVideo'])
   const svg = createMemo(() => getSVGData(props))
 
   return (
     <svg width='100%' {...rest} viewBox={`0 0 ${svg().length} ${svg().length}`}>
-      <path
-        d={svg().path}
-        fill={
-          svg().gradient || $props.fillImage
-            ? `url(#${svg().gradient ? `${svg().gradient?.attributes.id}` : svg().ids.image})`
-            : 'currentColor'
-        }
-      />
-
+      <g clip-path={`url(#${svg().id})`}>
+        <rect {...svg().cords} fill={svg().$gradient ? `url(#${svg().$gradient?.attributes?.id})` : 'currentColor'} />
+        <Show when={props.fillImage}>
+          <image
+            {...svg().cords}
+            href={props.fillImage}
+            // @ts-ignore
+            xlink:href={props.fillImage} // Note: Must use both href and xlinkHref to link a source
+            preserveAspectRatio='xMidYMid slice'
+          />
+        </Show>
+        <Show when={props.fillVideo}>
+          <foreignObject {...svg().cords}>
+            <div style={{ position: 'relative' }}>
+              <video src={props.fillVideo} width='100%' height='100%' muted autoplay style={{ 'object-fit': 'cover' }} />
+            </div>
+          </foreignObject>
+        </Show>
+      </g>
       <defs>
-        <Show
-          when={svg().gradient}
-          fallback={
-            <Show when={$props.fillImage}>
-              <pattern id={svg().ids.image} patternUnits='userSpaceOnUse' width='100%' height='100%'>
-                <image x='0' y='0' width='100%' height='100%' href={$props.fillImage} preserveAspectRatio='xMidYMid slice' />
-              </pattern>
-            </Show>
-          }
-        >
+        <clipPath id={svg().id}>
+          <path d={svg().path} />
+        </clipPath>
+        <Show when={svg().$gradient}>
           <Dynamic
-            component={svg().gradient?.isLinearGradient ? 'linearGradient' : 'radialGradient'}
-            {...svg().gradient?.attributes}
+            component={svg().$gradient?.isLinearGradient ? 'linearGradient' : 'radialGradient'}
+            {...svg().$gradient?.attributes}
           >
-            <For each={svg().gradient?.colors}>{({ color, offset }) => <stop offset={offset} stop-color={color} />}</For>
+            <For each={svg().$gradient?.colors}>{({ color, offset }) => <stop offset={offset} stop-color={color} />}</For>
           </Dynamic>
         </Show>
       </defs>
     </svg>
   )
 }
-
-// !Note: Must use both href and xlinkHref to link a source
