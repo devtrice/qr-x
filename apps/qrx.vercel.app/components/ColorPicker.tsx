8cd7f9225288a1b5d55ee9d2eafb457028bf6847
Damiano <htutwaiphyoe@gmail.com>
2024-06-20 14:56:47 +0700

    feat: color picker style

--- apps/qrx.vercel.app/components/ColorPicker.tsx
+++ apps/qrx.vercel.app/components/ColorPicker.tsx
@@ -7,21 +7,16 @@ type Props = { value: string; setValue: (x: string) => void }
 export default function ColorPicker({ value, setValue }: Props) {
   const ref = useRef(null)
   const [isShow, setShow] = useState(false)
-
-  useClickAway(ref, () => {
-    setShow(false)
-  })
+  useClickAway(ref, () => setShow(false))
 
   return (
     <div className='relative'>
-      <div
-        className='text-white border border-primary/50 focus-visible:border-primary focus-visible:bg-primary/20 bg-primary/10 rounded-xl w-full py-3 px-4'
-        onClick={() => setShow(true)}
-      >
+      <div className='text-white border border-primary/50 focus-visible:border-primary focus-visible:bg-primary/20 bg-primary/10 rounded-xl w-full py-3 px-4 flex justify-between items-center space-x-1'>
         {value}
+        <button onClick={() => setShow(true)} className='min-w-7 min-h-7 rounded-full' style={{ background: value }}></button>
       </div>
       {isShow && (
-        <div ref={ref} className='bg-black border border-white/25 shadow-md rounded-lg absolute z-10 p-2 mt-2'>
+        <div ref={ref} className='bg-primary/10 border border-primary/50 shadow-md rounded-lg absolute right-0 z-10 p-2 mt-2'>
           <BestColorPicker value={value} onChange={setValue} />
         </div>
       )}
