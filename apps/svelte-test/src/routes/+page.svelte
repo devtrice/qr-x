208fdeb07a922030475f2e13be62b14c86c13870
sudoaugustin <sudoaugustin@gmail.com>
2024-07-03 22:38:00 +0630

    remove: svelte under apps

--- apps/svelte-test/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import QRX from '@qr-x/svelte'
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<QRX data="https://kit.svelte.dev" shapes={{ eyeball: 'leaf', body: 'circle', eyeframe: 'rounded' }} />
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
