37550072823003fd54979f092f60284233555f44
sudoaugustin <sudoaugustin@gmail.com>
2024-07-05 22:04:36 +0630

    fix: bugs

--- packages/vue/components/CentralImage.vue
+++ packages/vue/components/CentralImage.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { ImgHTMLAttributes, defineProps } from 'vue'
 
-const props = withDefaults(defineProps<ImgHTMLAttributes>(), {
-  width: 28,
-  height: 28,
-})
+interface Props extends /* @vue-ignore */ ImgHTMLAttributes {}
+
+const props = withDefaults(defineProps<Props>(), { width: 28, height: 28 })
 </script>
 
 <template>
